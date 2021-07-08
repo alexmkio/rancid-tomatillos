a91215db29a762858ea0b0b0ad7c33a88a1a2192
Lauren Kessell <lkessell1@gmail.com>
2021-07-08 18:42:48 -0400

    Render movie details from props data

--- src/components/movie_details/MovieDetails.js
+++ src/components/movie_details/MovieDetails.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './MovieDetails.css';
 
-const MovieDetails = () => {
+const MovieDetails = ({ movie }) => {
   return (
-    <div>
-      <h3>I am MovieDetails</h3>
+    <div className='details-container'>
+      <img className='detail-background' src={movie['backdrop_path']} alt=''/>
+      <div className='detail-info'>
+        <h2 className='detail-title'>{movie.title}</h2>
+        <p>Release Date: {movie['release_date']}</p>
+        <p>Average User Rating: {movie['average_rating'].toFixed(2)}</p>
+        <button className='details-back'>Back</button>
+      </div>
     </div>
   )
 }
