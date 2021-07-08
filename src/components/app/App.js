05b5c492caf65cb5ada2c5e41806eda2ee056964
Alex Kio <alexandermartelkio@gmail.com>
2021-07-08 19:16:19 -0400

    Add conditional rendering with regards to error handling

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Posters from '../posters/Posters';
 import MovieDetails from '../movie_details/MovieDetails';
+import ErrorCode from '../error_code/ErrorCode';
 import movieData from '../../movieData';
 import { getApiData } from '../../apiCalls';
 import './App.css';
@@ -24,6 +25,20 @@ class App extends Component {
   }
 
   render() {
+    if (this.state.errorCode) {
+      return(
+        <ErrorCode code={this.state.errorCode}/>
+      )
+    }
+
+    if (!this.state.errorCode && !this.state.movies.length) {
+      return(
+        <>
+          <h1>Loading....</h1>
+        </>
+      ) 
+    }
+
     return(
       <>
       <header>
