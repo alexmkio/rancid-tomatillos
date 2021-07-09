9e92b8fa88dbdc25b158298ea2241edfb70e06eb
Alex Kio <alexandermartelkio@gmail.com>
2021-07-09 18:31:15 -0400

    Style error page

--- src/components/error_code/ErrorCode.js
+++ src/components/error_code/ErrorCode.js
@@ -5,11 +5,13 @@ import httpResponses from '../../httpResponses';
 const ErrorCode = ({code}) => {
   let response = httpResponses.find(response => response.code === parseInt(code))
   return (
-    <>
-      <h2>Error {response.code}</h2>
-      <h3>{response.message}</h3>
-      <p>{response.description}</p>
-    </>
+    <article className='error-container'>
+      <div className='error-div'>
+        <h2 className='error-h2'>Error {response.code}</h2>
+        <h3>{response.message}</h3>
+        <p>{response.description}</p>
+      </div>
+    </article>
   )
 }
 
