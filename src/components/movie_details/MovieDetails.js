b51d5b46c7b1bd30191e1a014177d0fc087f219a
Lauren Kessell <lkessell1@gmail.com>
2021-07-08 21:22:36 -0400

    Pass clearSelected as prop and add to button event handler

--- src/components/movie_details/MovieDetails.js
+++ src/components/movie_details/MovieDetails.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import './MovieDetails.css';
 
-const MovieDetails = ({ movie }) => {
+const MovieDetails = ({ movie, clearSelected }) => {
   return (
-    <div className='details-container'>
+    <section className='details-container'>
       <img className='detail-background' src={movie['backdrop_path']} alt=''/>
       <div className='detail-info'>
         <h2 className='detail-title'>{movie.title}</h2>
         <p>Release Date: {movie['release_date']}</p>
         <p>Average User Rating: {movie['average_rating'].toFixed(2)}</p>
-        <button className='details-back'>Back</button>
+        <button className='details-back' onClick={clearSelected}>Back</button>
       </div>
-    </div>
+    </section>
   )
 }
 
