42d7ccd4d933a666c7dca4433119036c1956fcbc
Alex Kio <alexandermartelkio@gmail.com>
2021-07-09 13:23:36 -0400

    Remove locally hosted movies data

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import Posters from '../posters/Posters';
 import MovieDetails from '../movie_details/MovieDetails';
 import ErrorCode from '../error_code/ErrorCode';
-import movieData from '../../movieData';
 import { getApiData } from '../../apiCalls';
 import './App.css';
 
@@ -10,8 +9,9 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      movies: movieData.movies,
+      movies: [],
       errorCode: null,
+      selectedMovie: null
     }
   }
 
@@ -21,15 +21,12 @@ class App extends Component {
       this.setState({movies: fetchedMovies.movies});
     } catch (e) {
       this.setState({errorCode: e.message})
-      selectedMovie: null
     }
   }
 
   selectMovie = (event) => {
     const match = this.state.movies.find(movie => movie.id === parseInt(event.target.id));
-    
     window.scrollTo(0,0);
-
     this.setState({ selectedMovie: match });
   }
 
@@ -59,7 +56,6 @@ class App extends Component {
       </header>
       <main>
         {!this.state.selectedMovie && <Posters movies={this.state.movies} selectMovie={this.selectMovie}/>}
-
         {this.state.selectedMovie && <MovieDetails movie={this.state.selectedMovie} clearSelected={this.clearSelected}/>}
       </main>
       </>
