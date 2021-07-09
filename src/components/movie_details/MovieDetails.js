5703ad8c7664eb1c8c16fc1afface88ef8111636
Lauren Kessell <lkessell1@gmail.com>
2021-07-09 10:27:14 -0400

    Improve semantic HTML

--- src/components/movie_details/MovieDetails.js
+++ src/components/movie_details/MovieDetails.js
@@ -6,9 +6,13 @@ const MovieDetails = ({ movie, clearSelected }) => {
     <section className='details-container'>
       <img className='detail-background' src={movie['backdrop_path']} alt=''/>
       <div className='detail-info'>
-        <h2 className='detail-title'>{movie.title}</h2>
-        <p>Release Date: {movie['release_date']}</p>
-        <p>Average User Rating: {movie['average_rating'].toFixed(2)}</p>
+        <dl>
+          <h2 className='detail-title'>{movie.title}</h2>
+          <dt>Release Date</dt>
+          <dd>{movie['release_date']}</dd>
+          <dt>Average User Rating</dt>
+          <dd>{movie['average_rating'].toFixed(2)}</dd>
+        </dl>
         <button className='details-back' onClick={clearSelected}>Back</button>
       </div>
     </section>
