6d64aac8e16ac21c8d9e8bf75bff9bd98e5110bf
Alex Kio <alexandermartelkio@gmail.com>
2021-07-09 18:27:13 -0400

    Create conditional logic to display posters or placeholders

--- src/components/posters/Posters.js
+++ src/components/posters/Posters.js
@@ -3,23 +3,43 @@ import Poster from '../poster/Poster';
 import './Posters.css';
 
 const Posters = ({movies, selectMovie}) => {
-  const posterCards = movies.map(movie => {
+  let placeholderIDs = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ]
+  const placeholderPosterCards = placeholderIDs.map(number => {
     return (
-      <Poster
-        key={movie.id}
-        id={movie.id}
-        photo={movie['poster_path']}
-        title={movie.title}
-        selectMovie={selectMovie}
-      />
+      <Poster key={number}/>
     )
   })
 
+  let posterCards = []
+  if (movies) {
+    posterCards = movies.map(movie => {
+      return (
+        <Poster
+          key={movie.id}
+          id={movie.id}
+          photo={movie['poster_path']}
+          title={movie.title}
+          selectMovie={selectMovie}
+        />
+      )
+    })
+  }
+
+  if (posterCards.length) {
+    return (
+      <>
+        <h2>Featured Films</h2>
+        <section className='poster-container'>
+          {posterCards}
+        </section>
+      </>
+    )
+  }
   return (
     <>
       <h2>Featured Films</h2>
       <section className='poster-container'>
-        {posterCards}
+        {placeholderPosterCards}
       </section>
     </>
   )
