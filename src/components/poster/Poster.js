7ba7bfd7eb5c1d2d93982403fe4075bb82b083f4
Alex Kio <alexandermartelkio@gmail.com>
2021-07-09 18:28:08 -0400

    Create conditional logic to build actual poster or placeholder

--- src/components/poster/Poster.js
+++ src/components/poster/Poster.js
@@ -2,16 +2,22 @@ import React from 'react';
 import './Poster.css';
 
 const Poster = ({photo, id, title, selectMovie}) => {
+  if (photo) {
+    return (
+      <article>
+        <img
+          className='poster-image'
+          id={id}
+          src={photo}
+          alt={title}
+          onClick={selectMovie}
+        />
+      </article>
+    )
+  }
   return (
-    <div>
-      <img
-        className='poster-image'
-        id={id}
-        src={photo}
-        alt={title}
-        onClick={selectMovie}
-      />
-    </div>
+    <article className='poster-image placeholder-height'>
+    </article>
   )
 }
 
