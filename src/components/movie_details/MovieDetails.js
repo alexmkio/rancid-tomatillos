dce32eafe7819cc04672b62437c1e7a5ea5ad263
Lauren Kessell <lkessell1@gmail.com>
2021-07-11 13:59:34 -0400

    Show genres and tagline on movie details page

--- src/components/movie_details/MovieDetails.js
+++ src/components/movie_details/MovieDetails.js
@@ -2,12 +2,21 @@ import React from 'react';
 import './MovieDetails.css';
 
 const MovieDetails = ({ movie, clearSelected }) => {
+  const genres = movie.genres.map(genre => <dd key={movie.genres.indexOf(genre)} className='genre'>{genre}</dd>);
+  const tagline = !movie.tagline ? null : <dd>"{movie.tagline}"</dd>;
+
   return (
     <section className='details-container'>
       <img className='detail-background' src={movie['backdrop_path']} alt=''/>
       <div className='detail-info'>
         <dl>
           <h2 className='detail-title'>{movie.title}</h2>
+          {tagline}
+          <dd>{movie.overview}</dd>
+          <dt>Genre</dt>
+          {genres}
+          <dt>Runtime</dt>
+          <dd>{movie.runtime} minutes</dd>
           <dt>Release Date</dt>
           <dd>{movie['release_date']}</dd>
           <dt>Average User Rating</dt>
