1bf7bd5a1c5a978f9731e53a6243b60effd31286
Lauren Kessell <lkessell1@gmail.com>
2021-07-11 14:09:36 -0400

    Add budget and revenue to details page

--- src/components/movie_details/MovieDetails.js
+++ src/components/movie_details/MovieDetails.js
@@ -3,7 +3,19 @@ import './MovieDetails.css';
 
 const MovieDetails = ({ movie, clearSelected }) => {
   const genres = movie.genres.map(genre => <dd key={movie.genres.indexOf(genre)} className='genre'>{genre}</dd>);
-  const tagline = !movie.tagline ? null : <dd>"{movie.tagline}"</dd>;
+  const tagline = !movie.tagline ? null : <dd className='tagline'>"{movie.tagline}"</dd>;
+  const budget = !movie.budget ? null : (
+    <>
+      <dt>Budget</dt>
+      <dd>${movie.budget}</dd>
+    </>
+  );
+  const revenue = !movie.revenue ? null : (
+    <>
+      <dt>Revenue</dt>
+      <dd>${movie.revenue}</dd>
+    </>
+  );
 
   return (
     <section className='details-container'>
@@ -21,6 +33,8 @@ const MovieDetails = ({ movie, clearSelected }) => {
           <dd>{movie['release_date']}</dd>
           <dt>Average User Rating</dt>
           <dd>{movie['average_rating'].toFixed(2)}</dd>
+          {budget}
+          {revenue}
         </dl>
         <button className='details-back' onClick={clearSelected}>Back</button>
       </div>
