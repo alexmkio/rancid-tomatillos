bf5da56e63013c76e624c11fc54e51f040b19b41
Lauren Kessell <lkessell1@gmail.com>
2021-07-14 18:22:42 -0400

    Add test for browser forward/back button navigation

--- cypress/integration/movie_details_spec.js
+++ cypress/integration/movie_details_spec.js
@@ -43,7 +43,15 @@ describe('Movie Details user flows', () => {
   it('Should return to the main page when the Back button is clicked', () => {
     cy.get('img[id="694919"]').click()
       .get('button').click()
-      .url().should('not.contain', '/694919')
+      .url().should('not.include', '/694919')
       .get('h2').contains('Featured Films')
+  });
+
+  it('Should be able to navigate using the browser forward/back buttons', () => {
+    cy.get('img[id="694919"]').click()
+      .go('back')
+      .url().should('not.include', '/694919')
+      .go('forward')
+      .url().should('include', '/694919')
   });
 })
