60389b8fdd1d76ac47a11dbebdd0e5e0621aa1d0
Lauren Kessell <lkessell1@gmail.com>
2021-07-13 20:46:34 -0400

    Add test for dashboard view with posters

--- cypress/integration/dashboard_spec.js
+++ cypress/integration/dashboard_spec.js
@@ -3,3 +3,13 @@ describe('Testing environment', () => {
     expect(true).to.equal(true);
   });
 });
+
+describe('Dashboard user flows', () => {
+  it('Should be able to visit http://localhost:3000 and see the header and list of movie posters', () => {
+    cy.visit('http://localhost:3000')
+      .get('h1').contains('Rancid Tomatillos')
+      .get('h2').contains('Featured Films')
+      .get('img[id="694919"]').should('be.visible')
+      .get('img[id="337401"]').should('be.visible')
+  });
+});
