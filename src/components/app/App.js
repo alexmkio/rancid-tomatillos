b9b22d11ee4b583c7ef7ab5e61279cf4331100cc
Alex Kio <alexandermartelkio@gmail.com>
2021-07-16 13:18:41 -0400

    Breakout cleanData method into utilities file

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,6 +3,7 @@ import Posters from '../posters/Posters';
 import MovieDetails from '../movie_details/MovieDetails';
 import ErrorCode from '../error_code/ErrorCode';
 import { getApiData } from '../../apiCalls';
+import { cleanData } from '../../utilities';
 import './App.css';
 import { Route } from 'react-router-dom';
 
@@ -24,34 +25,13 @@ class App extends Component {
     try {
       if (endPoint.includes('/')) {
         let fetchedMovie = await getApiData(endPoint)
-        this.setState({selectedMovie: this.cleanData(endPoint, fetchedMovie)})
+        this.setState({selectedMovie: cleanData(endPoint, fetchedMovie)})
       } else {
         let fetchedMovies = await getApiData(endPoint)
-        this.setState({movies: this.cleanData(endPoint, fetchedMovies)})
+        this.setState({movies: cleanData(endPoint, fetchedMovies)})
       }
     } catch (e) {
       this.setState({errorCode: e.message});
-    }
-  }
-
-  cleanData = (urlEndPoint, data) => {
-    if (urlEndPoint.includes('/')) {
-      return {average_rating: Math.round(data.movie.average_rating * 100) / 100,
-              backdrop_path: data.movie.backdrop_path,
-              budget: data.movie.budget.toLocaleString('en-US'),
-              genres: data.movie.genres,
-              overview: data.movie.overview,
-              release_date: data.movie.release_date,
-              revenue: data.movie.revenue.toLocaleString('en-US'),
-              runtime: data.movie.runtime,
-              tagline: data.movie.tagline,
-              title: data.movie.title}
-    } else {
-      return data.movies.map(movie => {
-        return {id: movie.id, 
-                poster_path: movie.poster_path, 
-                title: movie.title}
-      })
     }
   }
 
---
+++ src/utilities.js
@@ -0,0 +1,20 @@
+export const cleanData = (urlEndPoint, data) => {
+  if (urlEndPoint.includes('/')) {
+    return {average_rating: Math.round(data.movie.average_rating * 100) / 100,
+            backdrop_path: data.movie.backdrop_path,
+            budget: data.movie.budget.toLocaleString('en-US'),
+            genres: data.movie.genres,
+            overview: data.movie.overview,
+            release_date: data.movie.release_date,
+            revenue: data.movie.revenue.toLocaleString('en-US'),
+            runtime: data.movie.runtime,
+            tagline: data.movie.tagline,
+            title: data.movie.title}
+  } else {
+    return data.movies.map(movie => {
+      return {id: movie.id, 
+              poster_path: movie.poster_path, 
+              title: movie.title}
+    })
+  }
+};
