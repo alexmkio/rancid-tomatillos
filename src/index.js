b3fc6067da40c27aa6bd770ad088db643bfa88ca
Lauren Kessell <lkessell1@gmail.com>
2021-07-17 14:21:47 -0400

    Add hash router for GH Pages

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/app/App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
-const router = <BrowserRouter> <App /> </BrowserRouter>;
+const router = <HashRouter> <App /> </HashRouter>;
 
 ReactDOM.render(router, document.getElementById('root'));
