98b71176b4916720bd8adcc89fa2ffd4538bdea4
Alex Kio <alexandermartelkio@gmail.com>
2021-07-25 20:36:51 -0400

    Make fetch error check Class method

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,8 +1,3 @@
 export const getApiData = async (endPoint) => {
-  let response = await fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/${endPoint}`);
-  if (!response.ok) {
-    throw new Error(response.status);
-  }
-  let data = await response.json();
-  return data;
+  return await fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/${endPoint}`);
 };
